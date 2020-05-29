42cf74f0536b913892f37334ca9a0c090abfbb1c
Srasheed <61048530+Sumayya-maven@users.noreply.github.com>
2020-05-30 00:38:50 +0530

    api done

--- src/main/java/com/jkt/training/service/EmployeeService.java
+++ src/main/java/com/jkt/training/service/EmployeeService.java
@@ -1,5 +1,44 @@
 package com.jkt.training.service;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.jkt.training.entity.Employees;
+import com.jkt.training.repository.EmployeeRepository;
 
+@Service
 public class EmployeeService {
 
+  @Autowired
+	   private EmployeeRepository emprepo;
+	public List<Employees> getAllEmployees()
+	{
+		return emprepo.findAll();
+	}
+	
+	public Optional<Employees> getEmployee(int id)
+	{
+		Optional<Employees> emp = emprepo.findById(id);
+		return emp;
+		
+	}
+	public void addEmp(Employees employee)
+	{
+		emprepo.save(employee);
+	}
+	
+
+	public void updateEmp(Employees emp,int EmpId)
+	{
+	
+		if(emprepo.existsById(EmpId))
+			emprepo.save(emp);
+	
+	}
+	
+	public void deleteEmp(int EmpId)
+	{
+		Employees emp=emprepo.getOne(EmpId);
+		emprepo.delete(emp);
+	}
 }
